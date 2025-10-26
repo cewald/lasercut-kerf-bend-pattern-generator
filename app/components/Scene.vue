0c8cbb21a73f62d6a9969f070516b2b79977b272
cewald <hello@christianewald.de>
2025-10-26 11:31:12 +0100

    feat: add rectangle bounds and geometry calculations for dashed lines

--- app/components/Scene.vue
+++ app/components/Scene.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import * as THREE from 'three'
+
 const { dashLength, gapLength, numDashes, numLines, lineSpacing } = useDimensions()
 const { dashThickness } = useScene()
 
@@ -28,6 +30,43 @@ const dashedLines = computed(() => {
 
   return dashes
 })
+
+// Calculate rectangle bounds around the lines
+const rectangleBounds = computed(() => {
+  const maxDashes = numDashes.value
+  const dashesLength = maxDashes * dashLength.value
+  const gapsLength = (maxDashes - 1) * gapLength.value
+  const maxRowLength = dashesLength + gapsLength
+
+  const middleIndex = (numLines.value - 1) / 2
+  const topY = middleIndex * lineSpacing.value
+  const bottomY = -middleIndex * lineSpacing.value
+
+  const verticalPadding = 4 // 4 units spacing top and bottom
+
+  return {
+    width: maxRowLength,
+    height: topY - bottomY + verticalPadding * 2,
+    position: [0, 0, -0.01] as [number, number, number], // Slightly behind the dashes
+  }
+})
+
+// Create line geometry for rectangle outline
+const rectangleGeometry = computed(() => {
+  const { width, height } = rectangleBounds.value
+  const halfWidth = width / 2
+  const halfHeight = height / 2
+
+  const points = [
+    new THREE.Vector3(-halfWidth, halfHeight, 0),
+    new THREE.Vector3(halfWidth, halfHeight, 0),
+    new THREE.Vector3(halfWidth, -halfHeight, 0),
+    new THREE.Vector3(-halfWidth, -halfHeight, 0),
+    new THREE.Vector3(-halfWidth, halfHeight, 0), // Close the rectangle
+  ]
+
+  return new THREE.BufferGeometry().setFromPoints(points)
+})
 </script>
 
 <template>
@@ -43,5 +82,12 @@ const dashedLines = computed(() => {
       <TresBoxGeometry :args="dash.size" />
       <TresMeshBasicMaterial :color="0x000000" />
     </TresMesh>
+
+    <TresLine
+      :position="rectangleBounds.position"
+      :geometry="rectangleGeometry"
+    >
+      <TresLineBasicMaterial :color="0x000000" />
+    </TresLine>
   </TresScene>
 </template>
